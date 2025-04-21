baf77912f27ef0af783fc92dc90cca394e4aa450
Stuart Paterson <drskpaterson@gmail.com>
2025-04-21 10:59:15 +0100

    Added 2.7 and 2.8.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,16 +5,31 @@ const App = () => {
     { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const checkNameExists = (person,newName) => {
+    if (person.name === newName) return true;
+    return false
+  }
 
   const addName = (event) => {
     event.preventDefault()
+    // since persons contains non-primitives have to compare the string values explicitly rather than {name:"blah"}
+    // console.log(persons.includes({name: newName}))
+    // console.log(persons.includes(newName))
+    // console.log(persons)
+    if (persons.some((person) => checkNameExists(person,newName))) {
+      console.log("hi there same name")
+      alert(`${newName} is already added to phonebook`) 
+      return
+    }
     console.log('name, added', event.target)
     // console.log(newName)
     // console.log(persons)
     // here I encountered an interesting diversion where I'd put {newName} in the value and it
     // caused {name: {newName: "value"}} to render, meaning react couldn't render the child elements
-    console.log(persons.concat({ name: newName }))
-    setPersons(persons.concat({ name: newName }))
+    console.log(persons.concat({ name: newName, number: newNumber}))
+    setPersons(persons.concat({ name: newName, number: newNumber }))
   }
 
   const handleNameChange = (event) => {
@@ -22,9 +37,13 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const Person = ({ person }) => {
     // console.log('p',person)
-    return (<p>{person.name}</p>)
+    return (<p>{person.name} {person.number}</p>)
   }
 
   return (
@@ -39,8 +58,15 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
-          <div>debug: {newName}</div>
+          <div>debug: {newName} {newNumber}</div>
         </div>
       </form>
       <h2>Numbers</h2>
